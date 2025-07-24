6c195e4c714db53032678c3a0dcbb131033e4a8b
BELGNAOUI Othman <belgnaoui.othman@gmail.com>
2025-07-24 12:52:25 +0900

    MozaJoystickInput.cs has been modified to handle two joystick control. Still missing G1 and G2 input from adruino.

--- Assets/Underwater_BlueROV2/Inputs/MozaJoystickInput.cs
+++ Assets/Underwater_BlueROV2/Inputs/MozaJoystickInput.cs
@@ -4,8 +4,8 @@ using UnityEngine.InputSystem;
 namespace Underwater_BlueROV2
 {
     /// <summary>
-    /// Handles input from the Moza Racing joystick.
-    /// For now: assumes only one joystick is connected and duplicates its values.
+    /// Handles input from the Moza Racing joysticks.
+    /// Two joysticks are connected to control the ROV with two hands.
     /// G1 and G2 are set to zero (reserved for Arduino input). 
     /// TODO: Implement G1 and G2 with serial communication
     /// </summary>
@@ -13,23 +13,27 @@ namespace Underwater_BlueROV2
     {
         public override float[] GetFullInputVector()
         {
-            var joystick = Joystick.current;
-            if (joystick == null)
+            var joystickRight = Joystick.all[0]; // Right Joystick
+            var joystickLeft = Joystick.all[2];  //Left Joystick
+
+            if (joystickRight == null  || joystickLeft == null)
             {
-                Debug.LogWarning("No joystick detected.");
+                Debug.LogWarning("At least one joystick is not detected.");
                 return new float[6] { 0f, 0f, 0f, 0f, 0f, 0f };
             }
 
             // Read axes from the joystick (assumes Moza uses standard X/Y)
-            float x = joystick.stick.x.ReadValue(); // X1
-            float y = joystick.stick.y.ReadValue(); // Y1
+            float x1 = joystickRight.stick.x.ReadValue(); // X1
+            float y1 = joystickRight.stick.y.ReadValue(); // Y1
+            float x2 = joystickLeft.stick.x.ReadValue(); // X2
+            float y2 = joystickLeft.stick.y.ReadValue(); // Y2
 
             return new float[6]
             {
-                x,  // X1
-                x,  // X2 (duplicated)
-                y,  // Y1
-                y,  // Y2 (duplicated)
+                x1,  // X1
+                x2,  // X2 
+                y1,  // Y1
+                y2,  // Y2
                 0f, // G1 placeholder (Arduino)
                 0f  // G2 placeholder (Arduino)
             };
